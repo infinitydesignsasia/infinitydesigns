503ec8c14b2a133423ac5d10914f425bb811085b
infinitydesignsasia <121550771+infinitydesignsasia@users.noreply.github.com>
2024-02-21 21:17:25 +0530

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -10,8 +10,8 @@ export default function Hero() {
             <span className='max-w-[480px] mr-auto block text-3xl leading-[1]'></span>
           </h1>
           {/* <p className='text-sm md:text-xl text-white font-medium mt-8 z-30'>
-            Elevate your brand's image with our fashion-forward approach  Your vision, our expertise 
-            - the perfect fit
+            Elevate your brand's image with our fashion-forward approach  Your vision, our expertise-the perfect fit
+           
           </p> */}
           <div className='relative h-[54px] z-30 flex justify-start mt-5 items-center'>
             <button className='bg-[#a34e76] hover:bg-[#a1426e] text-white transition-all duration-300 px-4 py-2 rounded-lg'>

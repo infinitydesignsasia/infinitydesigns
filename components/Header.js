1ff42cea95441818de5ea96be510c6b9198b08d7
infinitydesignsasia <121550771+infinitydesignsasia@users.noreply.github.com>
2024-02-22 15:16:55 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -539,7 +539,7 @@ export default function Header() {
         <div className='py-2 flex justify-around animate-marquee whitespace-nowrap'>
           <span className='text-xl mx-4'>Redefining B2B fashion with every collection </span >
           <span className='text-xl mx-4 flex justify-center items-center'>
-     InnovateCollaborateDominate  
+                                          Innovate.Collaborate.Dominate  
           </span>
         </div>
       </div>
